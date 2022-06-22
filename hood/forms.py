0da342268ca2049c61a927bb870347c889fe4467
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2022-06-22 09:12:46 +0300

    create to be used in the application

---
+++ hood/forms.py
@@ -0,0 +1,36 @@
+from .models import Neighbourhood,Buisness,Post,Profile
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class UploadNewNeighbourhood(forms.ModelForm):
+    class Meta:
+        model=Neighbourhood
+        exclude=['occupant_count','admin']
+        fields=['hood','location','pic','description','hospital_number','police_number']
+
+class UploadNewBuisness(forms.ModelForm):
+    class Meta:
+        model=Buisness
+        fields=['name','owner','hood','image','email']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', )
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['pic', 'bio', 'hood', 'id_number','number', 'email']
